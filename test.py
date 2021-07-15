c4f014501e5a94c6bcf4a6cea4919421a167a254
JT Ives <jonc.ives@gmail.com>
2021-07-15 16:19:35 -0500

    e

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ class Process(Daemon):
 if __name__ == "__main__":
 	if len(sys.argv) == 2:
 		psu = Daemon('/tmp/pythond.pid', '/tmp/pythond.log')
-    	if 'start' == sys.argv[1]: psu.start()
-    	elif 'stop' == sys.argv[1]: psu.stop()
-    	elif 'restart' == sys.argv[1]: psu.restart()
+		if 'start' == sys.argv[1]: psu.start()
+		elif 'stop' == sys.argv[1]: psu.stop()
+		elif 'restart' == sys.argv[1]: psu.restart()
 	else: print("usage: %s start|stop|restart" % sys.argv[0])
