fd6524e50010ffb5b11097eb1804ab05856de8e6
JT Ives <jonc.ives@gmail.com>
2021-07-15 16:43:34 -0500

    testing suite finalized

--- pydaemon.py
+++ pydaemon.py
@@ -12,7 +12,7 @@ class Daemon:
 
     Usage: subclass this daemon class and override the run() method."""
 
-    def __init__(self, pidfile, logfile="", appname=""):
+    def __init__(self, pidfile, logfile, appname=""):
         self.appname = appname
         self.logfile = logfile
         self.pidfile = pidfile 
@@ -23,40 +23,49 @@ class Daemon:
         with open(self.logfile, write_flag) as logstr:
             logstr.write("\nPYDAEMON EXEC %s %s\n" % (datetime.now().strftime("%m:%d:%Y %H:%M:%S"), self.appname))
 
-    def logerr(self, msg):
-        if self.logfile:
-            with open(self.logfile, 'a') as logstr:
-                logstr.write("%s\n" % msg)
-        else: self.logerr(msg)
+    def log_error(self, msg):
+        with open(self.logfile, 'a') as logstr:
+            logstr.write("[ERROR] %s %s\n" % (datetime.now().strftime("%m:%d:%Y %H:%M:%S"), msg))
+
+    def log_debug(self, msg):
+        with open(self.logfile, 'a') as logstr:
+            logstr.write("[DEBUG] %s %s\n" % (datetime.now().strftime("%m:%d:%Y %H:%M:%S"), msg))
+
+    def log_status(self, msg):
+        with open(self.logfile, 'a') as logstr:
+            logstr.write("[STATUS] %s %s\n" % (datetime.now().strftime("%m:%d:%Y %H:%M:%S"), msg))            
+
+    def log_warning(self, msg):
+        with open(self.logfile, 'a') as logstr:
+            logstr.write("[WARNING] %s\t%s\n" % (datetime.now().strftime("%m:%d:%Y %H:%M:%S"), msg))
 
     def daemonize(self):
         """ Daemonize class. Employs UNIX double fork mechanism """
-        self.logerr("Daemonizing application")
+        self.log_status("daemonizing application...")
 
         try:
             pid = os.fork()
             if pid > 0: sys.exit(0)
-            self.logerr("Instantiated primary fork")
+            self.log_debug("instantiated primary fork")
         except OSError as err:
-            self.logerr('primary fork failed: {0}'.format(err))
+            self.log_error('primary fork failed: {0}'.format(err))
             sys.exit(1)
 
         # decouple from parent env
-        self.logerr("Decoupling process from parent environment")
         os.chdir('/')
         os.setsid()
         os.umask(0)
+        self.log_debug("decoupled process from parent environment")
 
         try:
             pid = os.fork()
             if pid > 0: sys.exit(0)
-            self.logerr("Instantiated redundant fork")
+            self.log_debug("instantiated redundant fork")
         except OSError as err:
-            self.logerr('redundant fork failed: {0}'.format(err))
+            self.log_error('redundant fork failed: {0}'.format(err))
             sys.exit(1)
 
         # redirect std file desc.
-        self.logerr("Redirecting standard file descriptors")
         sys.stdout.flush()
         sys.stderr.flush()
         si = open(os.devnull, 'r')
@@ -65,14 +74,15 @@ class Daemon:
         os.dup2(si.fileno(), sys.stdin.fileno())
         os.dup2(so.fileno(), sys.stdout.fileno())
         os.dup2(se.fileno(), sys.stderr.fileno())
+        self.log_debug("redirected standard file descriptors")
 
         # write pidfile
-        self.logerr("Registering process id")
+        self.log_debug("registering process id")
         atexit.register(self.delpid)
         pid = str(os.getpid())
         with open(self.pidfile, 'w') as pf:
             pf.write(pid + '\n')
-        self.logerr("Reserved process instance\nStarting application...")
+        self.log_status("starting application...")
 
     def delpid(self):
         os.remove(self.pidfile)
@@ -81,20 +91,19 @@ class Daemon:
         """ start the daemon """
 
         # if logging to file, append log header
-        if self.logfile: self.append_log_header()
-        self.logerr("Beginning Daemon Process")
+        self.append_log_header()
 
         try: # check for runstate with pidfile
             with open(self.pidfile, 'w+') as pf:
                 raw = pf.read().strip()
                 pid = int(raw) if raw.isdigit() else None
         except:
-            self.logerr("Verified unique application instance")
+            self.log_debug("verified unique application instance")
             pid = None
 
         if pid:
-            message = "pidfile {0} already exists. Daemon already running?\n"
-            self.logerr(message.format(self.pidfile))
+            message = "pidfile {0} already exists. Daemon already running?"
+            self.log_error(message.format(self.pidfile))
             sys.exit(1)
 
         # start the daemon
@@ -112,7 +121,7 @@ class Daemon:
 
         if not pid:
             message = "pidfile {0} does not exist. Daemon not running?\n"
-            self.logerr(message.format(self.pidfile))
+            self.log_warning(message.format(self.pidfile))
             return # usually happens in restarts -- not an error
         
         try: # try killing daemon process
@@ -137,4 +146,4 @@ class Daemon:
         def run(self):
             """ This needs to be overridden when Daemon is subclasses
             It will only be called after the process is daemonized by start() or restart()"""
-            self.logerr("Method 'run' must be overridden when Daemon is subclassed")
+            self.log_error("Method 'run' must be overridden when Daemon is subclassed")
--- test.py
+++ test.py
@@ -5,19 +5,18 @@ from pydaemon import Daemon
 class Process(Daemon):
 
 	def run(self):
-		while True: pass
-		# try:
-		# 	with open("testlog.txt", "w") as filestr:
-		# 		filestr.write("System daemon active")
-		# 	pid = os.fork()
-		# 	if pid == 0:
-		# 		count = 0
-		# 		while count < 10:
-		# 			with open("testlog.txt", "a") as filestr:
-		# 				filestr.write("Successful process fork iter")
-		# 			count += 1
-		# except Exception as err:
-		# 	self.logerr("Application error encountered: {0}".format(err))
+		try:
+			with open("testlog.txt", "w") as filestr:
+				filestr.write("System daemon active")
+			pid = os.fork()
+			if pid == 0:
+				count = 0
+				while count < 10:
+					with open("testlog.txt", "a") as filestr:
+						filestr.write("Successful process fork iter")
+					count += 1
+		except Exception as err:
+			self.logerr("Application error encountered: {0}".format(err))
 
 if __name__ == "__main__":
 	if len(sys.argv) == 2:
